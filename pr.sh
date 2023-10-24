c22315a032c4406a4d5f403629cbfcda4b3cfe53
CleverHolmes <hugo.rogers.16@gmail.com>
2023-10-24 10:49:27 -0700

    empty

--- pr.sh
+++ pr.sh
@@ -2,8 +2,13 @@ git config --local user.name CleverHolmes
 git config --local user.email hugo.rogers.16@gmail.com
 
 git checkout -B branch
+git add .
 git commit --allow-empty -m "empty"
 git push -u origin branch
+git checkout master
+
+echo asdf
+
 gh pr create -f
 gh pr merge --merge --delete-branch
 sleep 40
