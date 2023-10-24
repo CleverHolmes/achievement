4692f2ca4a08583bde7018dce3f04e53970ecf5e
CleverHolmes <hugo.rogers.16@gmail.com>
2023-10-24 12:21:55 -0700

    New commit message

--- pr-bot.py
+++ pr-bot.py
@@ -7,15 +7,23 @@ load_dotenv()
 
 name = os.environ.get("name")
 email = os.environ.get("email")
+repo = os.environ.get("repo")
 
+subprocess.run(['git', 'init'])
 subprocess.run(['git', 'config', '--local', 'user.name', name])
 subprocess.run(['git', 'config', '--local', 'user.email', email])
 
+try:
+    subprocess.check_output(['gh', 'repo', 'view', f'{name}/{repo}'])
+except subprocess.CalledProcessError:
+    subprocess.run(['gh', 'repo', 'create', repo, '--public'])
+
+
+
 subprocess.run(['git', 'checkout', '-B', 'new-branch'])
-# subprocess.run(['git', 'pull', 'origin', 'new-branch'])
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '--allow-empty', '-m' "New commit message"])
 subprocess.run(['git', 'push', '-u', 'origin', 'new-branch'])
 subprocess.run(['gh', 'pr', 'create', '-f'])
 subprocess.run(['gh', 'pr', 'merge', '--merge', '--delete-branch'])
-sleep(40)
+# sleep(40)
