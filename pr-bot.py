5795fd6d16c23b25c99753539d3dad14ebc5516c
CleverHolmes <hugo.rogers.16@gmail.com>
2023-10-24 12:28:22 -0700

    init

--- pr-bot.py
+++ pr-bot.py
@@ -9,21 +9,27 @@ name = os.environ.get("name")
 email = os.environ.get("email")
 repo = os.environ.get("repo")
 
-subprocess.run(['git', 'init'])
+# subprocess.run(['git', 'init'])
 subprocess.run(['git', 'config', '--local', 'user.name', name])
 subprocess.run(['git', 'config', '--local', 'user.email', email])
+subprocess.run(['git', 'remote', 'add', 'origin', f'https://github.com/CleverHolmes/{repo}.git'])
 
 try:
     subprocess.check_output(['gh', 'repo', 'view', f'{name}/{repo}'])
 except subprocess.CalledProcessError:
     subprocess.run(['gh', 'repo', 'create', repo, '--public'])
+    subprocess.run(['git', 'checkout', '-B', 'master'])
+    subprocess.run(['git', 'add', '.'])
+    subprocess.run(['git', 'commit', '-m', "init"])
+    subprocess.run(['git', 'push', '-u', 'origin', 'master'])
 
 
 
-subprocess.run(['git', 'checkout', '-B', 'new-branch'])
-subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '--allow-empty', '-m' "New commit message"])
-subprocess.run(['git', 'push', '-u', 'origin', 'new-branch'])
-subprocess.run(['gh', 'pr', 'create', '-f'])
-subprocess.run(['gh', 'pr', 'merge', '--merge', '--delete-branch'])
+
+# subprocess.run(['git', 'checkout', '-B', 'new-branch'])
+# subprocess.run(['git', 'add', '.'])
+# subprocess.run(['git', 'commit', '--allow-empty', '-m', "New commit message"])
+# subprocess.run(['git', 'push', '-u', 'origin', 'new-branch'])
+# subprocess.run(['gh', 'pr', 'create', '-f'])
+# subprocess.run(['gh', 'pr', 'merge', '--merge', '--delete-branch'])
 # sleep(40)
