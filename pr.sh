146ce262633e5a88f6ba5fd7f7affb5c70af8142
CleverHolmes <hugo.rogers.16@gmail.com>
2023-10-24 10:59:18 -0700

    empty

--- pr.sh
+++ pr.sh
@@ -2,6 +2,7 @@ git config --local user.name CleverHolmes
 git config --local user.email hugo.rogers.16@gmail.com
 
 git checkout -B branch
+git pull
 git add .
 git commit --allow-empty -m "empty"
 git push -u origin branch
